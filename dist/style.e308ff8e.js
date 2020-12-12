e7f45f121434e76e897ee121f9c91bd9eed3fd40
Tania <tania@DESKTOP-A941MPB.localdomain>
2020-12-12 19:13:59 +0100

    fotos arreglat header

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./assets/images/fonsM.jpg":[["fonsM.ec954a6e.jpg","assets/images/fonsM.jpg"],"assets/images/fonsM.jpg"],"./assets/images/fonsT.jpg":[["fonsT.af01cf4c.jpg","assets/images/fonsT.jpg"],"assets/images/fonsT.jpg"],"./assets/images/fonsPC.jpg":[["fonsPC.25d7cc04.jpg","assets/images/fonsPC.jpg"],"assets/images/fonsPC.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32887" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42887" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
